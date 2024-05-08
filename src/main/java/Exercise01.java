8d405423110fa45c76a2683267b25f2c8638c82f
Khoa <nvkhoa05@gmail.com>
2024-05-08 19:26:16 +0700

    Add more

--- src/main/java/Exercise01.java
+++ src/main/java/Exercise01.java
@@ -25,4 +25,31 @@ public class Exercise01 {
                 : "Phòng ban của nhân viên này là: " + account.department.name;
         System.out.println(message);
     }
+
+    void question04() {
+        Position position = new Position();
+        position.name = PositionName.TEST;
+
+        String message = (position.name == PositionName.DEV)
+                ? "Đây là Developer"
+                : "Người này không phải là Developer";
+        System.out.println(message);
+    }
+
+    void question07() {
+        Position position = new Position();
+        position.name = PositionName.TEST;
+
+        Account account = new Account();
+        account.position = position;
+
+        switch (account.position.name) {
+            case DEV:
+                System.out.println("Đây là Developer");
+                break;
+            default:
+                System.out.println("Người này không phải là Developer");
+                break;
+        }
+    }
 }
